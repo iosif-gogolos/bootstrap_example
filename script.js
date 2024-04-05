97c4dc2d0a5ff99ff79b5811236fe66278cafa98
iosif-gogolos <141866617+iosif-gogolos@users.noreply.github.com>
2024-04-05 13:51:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// For the JavaScript code
+@import "bootstrap/scss/bootstrap";
+
+$utilities: map-merge(
+  $utilities,
+  (
+    "cursor": (
+      property: cursor,
+      class: cursor,
+      responsive: true,
+      values: auto pointer grab,
+    )
+  )
+);
